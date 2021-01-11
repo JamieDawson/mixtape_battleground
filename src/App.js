7338e9e3ba81a9126de0c0dd4bad898c7b133375
JamieDawson <jamierobertdawson@gmail.com>
2021-01-10 22:19:49 -0800

    BandcampPlayer imported. I need to update the state with album IDs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import BandcampPlayer from 'react-bandcamp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//const value = 0;
+	const [albumId, setAlbumId] = useState(0);
+
+	return (
+		<div className='App'>
+			<div className='app__player'>
+				<BandcampPlayer album={albumId} size='large' />
+			</div>
+		</div>
+	);
 }
 
 export default App;
