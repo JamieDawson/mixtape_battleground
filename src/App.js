39db4415d0fc96a26cf5cf96a5b01cc3f516d4c2
JamieDawson <jamierobertdawson@gmail.com>
2021-01-23 23:33:20 -0800

    I can update album state for 1 album. Need to update code to handle more albums

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import './App.css';
 import React, {useState} from 'react';
 import BandcampPlayer from 'react-bandcamp';
+import {useForm} from 'react-hook-form';
 
 function App() {
 	//const value = 0;
-	const [albumId, setAlbumId] = useState(0);
+	const [albumId1, setAlbumId] = useState(0);
+	const [albumId2, setAlbumId] = useState(0);
+
+	const {register, handleSubmit} = useForm(); // initialize the hook
+
+	const onSubmit = (data) => {
+		//console.log(data);
+		const findAlbumNameInObject = data[Object.keys(data)[0]];
+		setAlbumId(findAlbumNameInObject);
+	};
 
 	return (
 		<div className='App'>
-			<div className='app__player'>
-				<BandcampPlayer album={albumId} size='large' />
+			<div className='app__all_albums'>
+				<div className='app__album'>
+					<BandcampPlayer album={albumId1} size='large' />
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<input name='firstAlbumSpot' ref={register} />{' '}
+						{/* register an input */}
+						<input type='submit' />
+					</form>
+				</div>
 			</div>
-			<button onClick={() => setAlbumId(3221399452)}>THE BUTTON</button>
+			<div className='app__all_albums'>
+				<div className='app__album'>
+					<BandcampPlayer album={albumId2} size='large' />
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<input name='firstAlbumSpot' ref={register} />{' '}
+						{/* register an input */}
+						<input type='submit' />
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 }
 
 export default App;
+
+{
+	/* <button onClick={() => setAlbumId(3221399452)}>THE BUTTON</button> */
+}
